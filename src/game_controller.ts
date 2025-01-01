6f1f95a45719f850df63627a620bb13803d179ec
Surlavi <Surlavi@users.noreply.github.com>
2025-01-01 22:25:40 +0900

    Separate game page logic to a independent file.

---
+++ src/game_controller.ts
@@ -0,0 +1,105 @@
+import { BoardUi,MoveDirection } from "./board_ui.js";
+import { Game } from "./game.js";
+
+
+function secondsToHumanReadable(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+export class GameController {
+  game: Game;
+  pageDom: HTMLElement;
+  boardUi: BoardUi;
+
+  constructor(game: Game, pageDom: HTMLElement) {
+    this.game = game;
+    this.pageDom = pageDom;
+
+    // FIXME: insert html here.
+
+    const boardDom = document.getElementById('board')!;
+    this.boardUi = new BoardUi(boardDom, game.puzzleBoard, {
+      size: boardDom.clientWidth,
+      highlightCursorNeighbors: true,
+      highlightNumber: true,
+      highlightNumberNeighbors: true,
+    });
+
+    this.refreshBanner();
+  }
+
+  handleKeyDownEvent(ev: KeyboardEvent) {
+    let direction: MoveDirection | null = null;
+    let value: number | null = null;
+
+    console.log(ev);
+
+    switch (ev.code) {
+      case 'ArrowUp':
+        direction = MoveDirection.UP;
+        break;
+      case 'ArrowDown':
+        direction = MoveDirection.DOWN;
+        break;
+      case 'ArrowLeft':
+        direction = MoveDirection.LEFT;
+        break;
+      case 'ArrowRight':
+        direction = MoveDirection.RIGHT;
+        break;
+      case 'Digit1':
+      case 'Digit2':
+      case 'Digit3':
+      case 'Digit4':
+      case 'Digit5':
+      case 'Digit6':
+      case 'Digit7':
+      case 'Digit8':
+      case 'Digit9':
+        value = parseInt(ev.code.charAt(5));
+        break;
+      case 'KeyD':
+        this.game.recalculateDraftNumbers();
+        this.boardUi.updateBoard();
+        break;
+      default:
+        // Do nothing now.
+        break;
+    }
+    if (direction !== null) {
+      this.boardUi.moveCursor(direction);
+      ev.preventDefault();
+    }
+    if (value !== null && this.boardUi.cursorCoord !== null) {
+      console.log('hit %d', value);
+      if (ev.shiftKey) {
+        this.game.fillInNumber(this.boardUi.cursorCoord, value);
+      } else {
+        this.game.toggleDraftNumber(this.boardUi.cursorCoord, value);
+      }
+      this.boardUi.updateBoard();
+      this.refreshBanner(true);
+    }
+  }
+
+  refreshBanner(once = false) {
+    console.log('called');
+
+    const timerDom = document.getElementById('value-timer')!;
+    const mistakesDom = document.getElementById('value-mistakes')!;
+    const remainingDom = document.getElementById('value-remaining')!;
+
+    timerDom.textContent = `${secondsToHumanReadable(this.game.getElapsedSeconds())}`;
+    mistakesDom.textContent = `${this.game.mistakes}`;
+    remainingDom.textContent = `${this.game.getEmptyCellsCount()}`;
+
+    // TODO: Add stop condition.
+    if (!once) {
+      setTimeout(this.refreshBanner, 1000);
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import init, * as wasm from '../wasm/pkg/sudoku_wasm.js';
-import {BoardUi, MoveDirection} from './board_ui.js';
 import {getCurrentTheme} from './theme.js';
 import {GenericBoard} from './types.js';
+import {GameController} from './game_controller.js';
 import {Game} from './game.js';
 
 function startUi() {
@@ -12,16 +12,11 @@ function startUi() {
   }
 
   const appContainerDom = document.getElementById('app-container');
-  const gamePageDom = document.getElementById('game-page');
+  const gamePageDom = document.getElementById('game-page')!;
 
+  // FIXME: remove this?
   gamePageDom!.style.height = `${appContainerDom!.clientHeight}px`;
   console.log('setting height to %d', appContainerDom!.clientHeight);
-
-  const boardDom = document.getElementById('board');
-  if (boardDom === null) {
-    console.log('Failed to find the dom node');
-    return;
-  }
 
   // Create a random game.
   const puzzleArr = new Uint8Array(81);
@@ -34,104 +29,22 @@ function startUi() {
 
   document.body.style.backgroundColor = getCurrentTheme().color_background;
 
-  // const answer = GenericBoard.createBoardFromString(ANSWER);
-  // const puzzle = GenericBoard.createBoardFromString(BOARD_EXAMPLE);
   const answer = GenericBoard.createBoardFromString(
     answerArr.join('').replaceAll('0', '.'),
   );
   const puzzle = GenericBoard.createBoardFromString(
     puzzleArr.join('').replaceAll('0', '.'),
   );
+
   const game = new Game(answer, puzzle);
-  const boardUi = new BoardUi(boardDom, game.puzzleBoard, {
-    size: boardDom.clientWidth,
-    highlightCursorNeighbors: true,
-    highlightNumber: true,
-    highlightNumberNeighbors: true,
-  });
+  const controller = new GameController(game, gamePageDom);
 
   gamePageDom!.style.opacity = '1';
 
-  function refreshBanner(once = false) {
-    console.log('called');
-
-    const timerDom = document.getElementById('value-timer')!;
-    const mistakesDom = document.getElementById('value-mistakes')!;
-    const remainingDom = document.getElementById('value-remaining')!;
-
-    timerDom.textContent = `${secondsToHumanReadable(game.getElapsedSeconds())}`;
-    mistakesDom.textContent = `${game.mistakes}`;
-    remainingDom.textContent = `${game.getEmptyCellsCount()}`;
-
-    // TODO: Add stop condition.
-    if (!once) {
-      setTimeout(refreshBanner, 1000);
-    }
-  }
-
-  refreshBanner();
-
-  window.addEventListener('keydown', (ev: KeyboardEvent) => {
-    let direction: MoveDirection | null = null;
-    let value: number | null = null;
-
-    console.log(ev);
-
-    switch (ev.code) {
-      case 'ArrowUp':
-        direction = MoveDirection.UP;
-        break;
-      case 'ArrowDown':
-        direction = MoveDirection.DOWN;
-        break;
-      case 'ArrowLeft':
-        direction = MoveDirection.LEFT;
-        break;
-      case 'ArrowRight':
-        direction = MoveDirection.RIGHT;
-        break;
-      case 'Digit1':
-      case 'Digit2':
-      case 'Digit3':
-      case 'Digit4':
-      case 'Digit5':
-      case 'Digit6':
-      case 'Digit7':
-      case 'Digit8':
-      case 'Digit9':
-        value = parseInt(ev.code.charAt(5));
-        break;
-      case 'KeyD':
-        game.recalculateDraftNumbers();
-        boardUi.updateBoard();
-        break;
-      default:
-        // Do nothing now.
-        break;
-    }
-    if (direction !== null) {
-      boardUi.moveCursor(direction);
-      ev.preventDefault();
-    }
-    if (value !== null && boardUi.cursorCoord !== null) {
-      console.log('hit %d', value);
-      if (ev.shiftKey) {
-        game.fillInNumber(boardUi.cursorCoord, value);
-      } else {
-        game.toggleDraftNumber(boardUi.cursorCoord, value);
-      }
-      boardUi.updateBoard();
-      refreshBanner(true);
-    }
+  // We can add logic to remove this listener when necessary in the future.
+  window.addEventListener('keydown', ev => {
+    controller.handleKeyDownEvent(ev);
   });
-}
-
-function secondsToHumanReadable(seconds: number): string {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = Math.floor(seconds % 60);
-  const formattedMinutes = String(minutes).padStart(2, '0');
-  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-  return `${formattedMinutes}:${formattedSeconds}`;
 }
 
 async function main() {
